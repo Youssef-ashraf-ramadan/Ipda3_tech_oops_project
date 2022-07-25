23a2eb6fbfd1f4bd10f3dbe59ab7654e9f07b41e
Youssef-ashraf-ramadan <youssefashrafr@hotmail.com>
2022-07-25 07:53:15 +0200

    last-commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,11 @@
 
 
 let buttons = document.querySelectorAll('.page-item');
-let carditem = document.querySelectorAll('.cards-item')
+let carditem = document.querySelectorAll('.cards-item');
 
-let entercode = document.querySelector('.entercode')
-let loginbutton = document.querySelector('.login-link')
-
+let entercode = document.querySelector('.entercode');
+let loginbutton = document.querySelector('.login-link');
+let carselect = document.querySelector('.car-select')
 for(let i = 0; i < buttons.length; i++){
 buttons[i].addEventListener('click' , function(){
     for(let j = 0;  j < carditem.length; j++){
@@ -21,6 +21,15 @@ buttons[i].addEventListener('click' , function(){
 $('.fa-star').click(function() {
     $(this).toggleClass('far fas');
   })
+  var data;
+  $('.car-select').change(function() {
+    if(carselect.value != "عربيات"){
+        data = $('.information-select').html();
+        $('.information-select').empty();
+    }else{
+        $('.information-select').html(data);
+    }
+})
 // prevent default from login if input is empty 
 loginbutton.addEventListener('click' , function(e){
     if(entercode.value == ""){
